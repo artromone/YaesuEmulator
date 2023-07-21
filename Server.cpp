e252064a1c0531254cf6dd32a1a4a8083e199318
Kirill Cherniakov <hatecraft587@gmail.com>
2023-07-21 14:44:38 +0300

    change work process for server

--- Server.cpp
+++ Server.cpp
@@ -1,32 +1,37 @@
 #include "Server.h"
-#include <QTcpSocket>
-#include <QDebug>
-#include <memory>
+
 #include "Settings.h"
 
-TcpServer::TcpServer(QObject *parent): QObject(parent)
+#include <QDebug>
+#include <QTcpSocket>
+
+#include <memory>
+
+TcpServer::TcpServer(QObject* parent) : QObject(parent)
 {
-  Settings * settings = new Settings();
-  server_ = new QTcpServer();
+    Settings* settings = new Settings();
+    server_ = new QTcpServer();
 
-  QObject::connect(server_, &QTcpServer::newConnection,
-                   this, &TcpServer::newConnection);
+    QObject::connect(server_, &QTcpServer::newConnection, this, &TcpServer::newConnection);
 
-  if (server_->listen(settings->address, settings->port))
-  {
-    qDebug() << "Server started.";
-  }
-  else
-  {
-    qDebug() << "Server could not start.";
-  }
+    if (!server_->listen(settings->address, settings->port))
+    {
+        qDebug() << "Server could not start.";
+    }
+    else
+    {
+        qDebug() << "Server started.";
+    }
 }
 
 void TcpServer::newConnection()
 {
-  auto socket = std::make_shared<QTcpSocket>(server_->nextPendingConnection());
-  qDebug() << "Hello client.";
-  socket->write("Hello client.");
-  //socket->flush();
-  socket->waitForBytesWritten(3000);
+    auto socket = server_->nextPendingConnection();
+
+    QObject::connect(socket, &QTcpSocket::disconnected, this, [&] { delete socket; });
+
+    socket->write("Hello client.\n");
+    //    socket->flush();
+    //    socket->waitForBytesWritten(3000);
+    //    socket->close();
 }
